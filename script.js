e2ae135724ea6b7b60f0cc3e5f43427a4e1fe20f
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-01 11:43:16 -0400

    ...

--- script.js
+++ script.js
@@ -11,11 +11,7 @@ function generateRandomString() {
 
 window.onload = () => {
   const fragment = new URLSearchParams(window.location.hash.slice(1));
-  const [accessToken, tokenType, state] = [
-    fragment.get('access_token'),
-    fragment.get('token_type'),
-    fragment.get('state'),
-  ];
+	const [accessToken, tokenType, state] = [fragment.get('access_token'), fragment.get('token_type'), fragment.get('state')];
 
   if (!accessToken) {
     const randomString = generateRandomString();
